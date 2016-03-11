5cc4c6220d2b7c2383f25c1b6ebcfa95d0a4ba6a
TJ Madsen <tjmadsen@gmail.com>
2016-03-12 04:39:16 +0800

    fixed response, check for token

--- index.js
+++ index.js
@@ -13,34 +13,21 @@ app.use(bodyParser.json());
 app.post('/rallybot', function(req, res){
   if( !(req.body && req.body instanceof Object && Object.keys(req.body).length > 0) ) return res.send(403);
 
-    var json = {text: 'Test Response', username: 'rallybot', icon_emoji: ':nerd:'};
-
-
-    if(data.channel_id != null){
-        json.channel = data.channel_id; 
-    }else if(data.team_id != null){
-        json.team = data.team_id; 
+    if(req.body.token == process.env.BOT_TOKEN){
+        var json = {text: 'Test Response', username: 'rallybot', icon_emoji: ':nerd:'};
+        if(req.body.channel_id != null){
+            json.channel = req.body.channel_id; 
+        }else if(req.body.team_id != null){
+            json.team = req.body.team_id; 
+        }
+        res.send(json);
+    }else{
+        res.send('Bad!')
     }
-
-  
-  // var json = {
-  //   text: text,
-  //   channel: '#test-int',
-  //   username: 'rallybot'
-  // };
-
-
-  // slackhook.send(json, function(err, res){
-  //   console.log(err, res);
-  // });
-
-  // res.body('Test');
-
-  res.send(json);
 });
 
 app.get('/', function(req, res){
-  res.send(200);
+  res.sendStatus(200);
 });
 
 app.listen(process.env.PORT || 3232);
