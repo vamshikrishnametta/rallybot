38d189d20252c7e61e8d0a1345047ebb51c5f5c5
TJ Madsen <tjmadsen@gmail.com>
2016-03-12 04:37:07 +0800

    fix scope of response

--- index.js
+++ index.js
@@ -13,15 +13,16 @@ app.use(bodyParser.json());
 app.post('/rallybot', function(req, res){
   if( !(req.body && req.body instanceof Object && Object.keys(req.body).length > 0) ) return res.send(403);
 
-  var teamcityText = req.body.build ? req.body.build.text : 'No text from teamcity';
-  var buildStatus = req.body.build ? req.body.build.buildStatus : 'No build status';
-  var buildLink = req.body.build ? req.body.build.buildStatusUrl : 'No build link';
+    var json = {text: 'Test Response', username: 'rallybot', icon_emoji: ':nerd:'};
 
-  var text = teamcityText;
-  text += '\nStatus: ' + buildStatus;
-  text += '\n<' + buildLink + '|Build>';
 
-  var json = {text: 'Hi ' + req.body.user_name, username: 'rallybot', icon_emoji: ':nerd:'};
+    if(data.channel_id != null){
+        json.channel = data.channel_id; 
+    }else if(data.team_id != null){
+        json.team = data.team_id; 
+    }
+
+  
   // var json = {
   //   text: text,
   //   channel: '#test-int',
