b615e46ef8f1bc06c311a05de5157b42641ec691
TJ Madsen <tjmadsen@gmail.com>
2016-03-12 03:33:47 +0800

    changed app

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
+
+
+
+// app.post('/rallybot', function(req, res){
+//     var hook = slack.respond(req.body);
+//     res.json({text: 'Hi ' + hook.user_name, username: 'rallybot', icon_emoji: ':nerd:'});
+// });
+
+
+var express = require('express');
 var Slackhook = require('slackhook');
-var slack = new Slackhook({
+var slackhook = new Slackhook({
     domain: process.env.DOMAIN,
     token: process.env.BOT_TOKEN
 }); 
+var app = express();
 
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.post('/rallybot', function(req, res){
-    var hook = slack.respond(req.body);
-    res.json({text: 'Hi ' + hook.user_name, username: 'rallybot', icon_emoji: ':nerd:'});
+  if( !(req.body && req.body instanceof Object && Object.keys(req.body).length > 0) ) return res.send(403);
+
+  var teamcityText = req.body.build ? req.body.build.text : 'No text from teamcity';
+  var buildStatus = req.body.build ? req.body.build.buildStatus : 'No build status';
+  var buildLink = req.body.build ? req.body.build.buildStatusUrl : 'No build link';
+
+  // var text = teamcityText;
+  // text += '\nStatus: ' + buildStatus;
+  // text += '\n<' + buildLink + '|Build>';
+
+  var json = {text: 'Hi ' + hook.user_name, username: 'rallybot', icon_emoji: ':nerd:'};
+
+  slackhook.send(json, function(err, res){
+    console.log(err, res);
+  });
+
+  res.send(200);
 });
+
+app.get('/', function(req, res){
+  res.send(200);
+});
+
+app.listen(process.env.PORT || 3232);
+console.log('App listening on port ' + (process.env.port || 3232) );
