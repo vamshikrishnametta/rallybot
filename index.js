1669e539c7394915ef0df801abef836c42df2359
TJ Madsen <tjmadsen@gmail.com>
2016-03-12 05:18:27 +0800

    added Rally

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var express = require('express'),
     bodyParser = require('body-parser');
+var rally = require('rally');
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+var restApi = rally({
+       apiKey: process.env.RALLY_API_KEY, //preferred, required if no user/pass, defaults to process.env.RALLY_API_KEY
+       apiVersion: 'v2.0', //this is the default and may be omitted
+       requestOptions: {
+           headers: {
+               'X-RallyIntegrationName': 'slack integration',  //while optional, it is good practice to
+               'X-RallyIntegrationVendor': 'tj madsen',             //provide this header information
+               'X-RallyIntegrationVersion': '1.0'                    
+           }
+           //any additional request options (proxy options, timeouts, etc.)     
+       }
+   });
+
 
 app.post('/rallyslash', function(req, res){
   if( !(req.body && req.body instanceof Object && Object.keys(req.body).length > 0) ) return res.send(403);
@@ -12,24 +27,44 @@ app.post('/rallyslash', function(req, res){
 
     if(req.body.token == process.env.BOT_TOKEN){
         var message = '';
+        var json = json = {text: message, username: 'rallybot', icon_emoji: ':nerd:'};
         var text = req.body.text;
         var tokens = text.split(" ");
         if(tokens[0] == 'help'){
-            message = 'Use format: /rally US123 action\n\n Possible Actions: \n status \n description \n link \n notes';
-        }else if(tokens[0].substr(0,2) == 'US'){
-            
-
+            json.message = 'Use format: /rally US123 action\n\n Possible Actions: \n status \n description \n link \n notes';
+            res.send(json);
+        }else if((tokens[0].substr(0,2) == 'US' || tokens[0].substr(0,2) == 'DE') && !isNaN(tokens[0].substring(1,tokens[0].lenth - 1))){
+            restApi.get({
+                ref: tokens[0], //may be a ref ('/defect/1234') or an object with a _ref property
+                fetch: ['FormattedID', 'Name', 'Description'], //fields to fetch
+                // scope: {
+                    // workspace: '/workspace/12345' //optional, only required if reading in non-default workspace
+                // },
+                requestOptions: {} //optional additional options to pass through to request
+            }).then(function(result) {
+                console.log(result.Object);
+                if(token[1] == 'description'){
+                    json.message = result.Object.Description    
+                }else{
+                    json.message = '<https://rally1.rallydev.com/#/'+process.env.RALLY_WORKSPACE+'/search?keywords='+token[0]+'>'
+                }
+                
+            res.send(json);
+            }).fail(function(errors) {
+                console.log(errors);
+            });
 
         }else(){
-            message = 'Use format: /rally US123 action'
+            json.message = 'Use format: /rally US123 action'
+            res.send(json);
         }
 
 
-        var json = {text: message, username: 'rallybot', icon_emoji: ':nerd:'};
-        if(req.body.channel_id != null && req.body.channel_name != 'directmessage'){
-            json.channel_id = req.body.channel_id; 
-        }
-        res.send(json);
+        // var json = {text: message, username: 'rallybot', icon_emoji: ':nerd:'};
+        // if(req.body.channel_id != null && req.body.channel_name != 'directmessage'){
+        //     json.channel_id = req.body.channel_id; 
+        // }
+        // res.send(json);
     }else{
         res.send('Bad!')
     }
