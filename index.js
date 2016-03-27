4c784c11a695efd71aa237791339f462ac2cdbb4
TJ Madsen <tjmadsen@gmail.com>
2016-03-27 20:17:30 +0800

    Added support for multi user

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var pg = require('pg');
+var redis = require('redis');
+var bluebird = require('bluebird');
 var express = require('express'),
     bodyParser = require('body-parser');
 var rally = require('rally'),
@@ -7,20 +10,37 @@ var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+                  if (err) throw err;
+                  console.log('Connected to postgres! Getting schemas...');
 
-var restApi = rally({
+                  client
+                    .query('CREATE TABLE user ('+
+                           '   username    varchar(255) CONSTRAINT firstkey PRIMARY KEY,'+
+                           '   apiKey      varchar(64) NOT NULL,'+
+                          ');')
+                    .on('end', function() { client.end(); })
+                });
+
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+var redisClient = require('redis').createClient(process.env.REDIS_URL);
+
+var rallyConnectionInfo = {
        // apiKey: process.env.RALLY_API_KEY, //preferred, required if no user/pass, defaults to process.env.RALLY_API_KEY
        apiVersion: 'v2.0', //this is the default and may be omitted
        server: 'https://rally1.rallydev.com',
        requestOptions: {
            headers: {
-               'X-RallyIntegrationName': 'slack integration',  //while optional, it is good practice to
-               'X-RallyIntegrationVendor': 'tj madsen',             //provide this header information
+               'X-RallyIntegrationName': 'Rally Slack Integration',  //while optional, it is good practice to
+               'X-RallyIntegrationVendor': 'TJ Madsen',             //provide this header information
                'X-RallyIntegrationVersion': '1.0'                    
            }
            //any additional request options (proxy options, timeouts, etc.)     
        }
-   });
+   };
 
 var removeHTML = function(inText){
   var outText = inText;
@@ -51,14 +71,16 @@ var removeHTML = function(inText){
 
 
 app.post('/rallyslash', function(req, res){
-  if( !(req.body && req.body instanceof Object && Object.keys(req.body).length > 0) ) return res.send(403);
+  
+  delete rallyConnectionInfo.apiKey;
 
-    //
+  if( !(req.body && req.body instanceof Object && Object.keys(req.body).length > 0) ) return res.send(403);
 
     if(req.body.token == process.env.BOT_TOKEN){
         var message = ' ';
         var json = json = {text: message, username: 'rallybot', icon_emoji: ':nerd:', response_type: 'ephemeral'};
         var text = req.body.text;
+        var username = req.body.user_name;
         console.log(req.body);
         var tokens = text.split(" ");
         var type = '';
@@ -68,9 +90,65 @@ app.post('/rallyslash', function(req, res){
           var number = tokens[0].substring(2,tokens[0].length);
         }
         if(tokens.length >= 1 && tokens[0] == 'help'){
-            json.message = 'Use format: /rally US123 action\n\n Possible Actions: \n status \n description \n link \n notes';
+            json.message = 'Use format: /rally US123 action\n\n Possible Actions: \n status \n description \n link \n notes \n design';
             res.send(json);
+        }else if(tokens.length >= 1 && tokens[0] == 'register'){
+            if(tokens.length == 2){
+              rallyConnectionInfo.apiKey = tokens[1];
+              var rallyReqBody = {
+                  type: 'hierarchicalrequirement',
+                  query: queryUtils.where('FormattedID', '=', 'US1'),
+                  fetch: ['FormattedID', 'Name', 'Description', 'Notes', 'CommExOwner', 'UserStoryStatus', 'CommexITOwner', 'AssignedArchitect', 'DesignState', 'Blocked', 'BlockedReason', 'ScheduleState', 'Iteration', 'Release', 'DeveloperAssigned1', 'SystemDesignSuggestions' ], //fields to fetch
+                  limit: Infinity,
+                  order: 'Rank',
+                  requestOptions: {} //optional additional options to pass through to request
+              };
+              restApi.query(rallyReqBody).then(function(result) {
+                pg.connect(process.env.DATABASE_URL, function(err, client) {
+                  if (err) throw err;
+                  console.log('Connected to postgres! Getting schemas...');
+
+                  client
+                    .query('INSERT INTO user (username, apiKey) VALUES (\''+username+'\',\''+tokens[1]+'\') ON CONFLICT (username) DO UPDATE user SET apiKey = '+tokens[1]+' WHERE username = '+username+';')
+                    .on('end', function() { 
+                      redisClient.set("username", tokens[1]);
+                      client.end(); 
+                    })
+                });
+              }).fail(function(errors) {
+                json.message = 'Invalid API Key';
+                res.send(json);
+              });
+            }else{
+              json.message = 'Use format: /rally register [API_KEY]';
+              res.send(json);
+            }
         }else if(tokens.length >= 2 && (type == 'US' || type == 'DE') && !isNaN(number)){
+
+            //Get Connection API
+
+            redisClient.getAsync('username').then(function(res) {
+              if(res == null){
+                pg.connect(process.env.DATABASE_URL, function(err, client) {
+                  if (err) throw err;
+                  console.log('Connected to postgres! Getting schemas...');
+
+                  client
+                    .query('SELECT apiKey FROM user WHERE username = '+username+';')
+                    .on('row', function(row) {
+                      if(row.apiKey && row.apiKey != null){
+                        redisClient.set("username", row.apiKey);
+                        rallyConnectionInfo.apiKey = row.apiKey;
+                      }
+                    });
+                });
+              }else{
+                rallyConnectionInfo.apiKey = res;
+              }
+            }
+
+            var restApi = rally(rallyConnectionInfo);
+
             var rallyReqBody = {
                 type: 'hierarchicalrequirement',
                 query: queryUtils.where('FormattedID', '=', tokens[0]),
